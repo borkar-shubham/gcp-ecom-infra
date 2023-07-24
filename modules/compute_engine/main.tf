dd72b35bd366a73b9e0e7f93fa31878a7932a02f
Shubham Borkar <shubham@MacBook-Pro.local>
2023-07-24 18:02:48 +0530

    added advanced code for GCE

--- modules/compute_engine/main.tf
+++ modules/compute_engine/main.tf
@@ -1,36 +1,10 @@
-# data "google_compute_network" "default" {
-#   name                    = "default"
-# }
-
-# ## SUBNET
-# resource "google_compute_subnetwork" "subnet-1" {
-#   name                     = var.subnet-name
-#   ip_cidr_range            = var.subnet-cidr
-#   network                  = data.google_compute_network.default.self_link
-#   region                   = var.region
-#   private_ip_google_access = var.private_google_access
-# }
-
-# resource "google_compute_firewall" "default" {
-#   name    = "test-firewall"
-#   network = data.google_compute_network.default.self_link
-
-#   allow {
-#     protocol = "icmp"
-#   }
-
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80", "8080", "1000-2000", "22"]
-#   }
-
-#   source_tags = ["web"]
-# }
-
-resource "google_compute_instance" "nginx-proxy" {
-  name         = var.compute_instance_name
-  machine_type = var.machine_type
+resource "google_compute_instance" "compute_engine_pub" {
+  name         = format("%s-%s-server", var.project_name, var.environment_map[var.target_env])
+  machine_type = var.machine_types[var.target_env]
   zone         = var.zone_name
+  labels = {
+    environment = var.environment_map[var.target_env]
+  }
 
   tags = var.tags
 
@@ -42,5 +16,31 @@ resource "google_compute_instance" "nginx-proxy" {
 
   network_interface {
     network = var.network_interface
+    
+    access_config { 
+      #external_static_ip   ....this block will add a external ip to the machine
+    }
   }
 }
+
+# resource "google_compute_instance" "compute_engine_pvt" {
+#   name         = format("%s-%s-server", var.project_name, var.environment_map[var.target_env])
+#   machine_type = var.machine_types[var.target_env]
+#   zone         = var.zone_name
+#   labels = {
+#     environment = var.environment_map[var.target_env]
+#   }
+
+#   tags = var.tags
+
+#   boot_disk {
+#     initialize_params {
+#       image = var.image
+#       }
+#     }
+
+#   network_interface {
+#     network = var.network_interface
+#   }
+# }
+
--- modules/compute_engine/variables.tf
+++ modules/compute_engine/variables.tf
@@ -1,11 +1,31 @@
-variable "compute_instance_name" {
+variable "project_name" {
   type = string
-  default = "web-server"
+  default = "gcp-ecom"
 }
 
-variable "machine_type" {
+variable "target_env" {
   type = string
-  default = "e2-medium"
+  default = "DEV"
+}
+
+variable "environment_map" {
+  type = map(string)
+  default = {
+    "DEV" = "dev",
+    "QA" = "qa",
+    "STAGE" = "stage",
+    "PROD" = "prod"
+  }
+}
+variable "machine_types" {
+  type = map(string)
+  default = {
+    "DEV" = "e2-micro"
+    "QA" = "e2-small"
+    "STAGE" = "e2-medium"
+    "PROD" = "e2-standard-2"
+
+  }
 }
 
 variable "zone_name" {
@@ -27,3 +47,25 @@ variable "network_interface" {
   type = string
   default = "default"
 }
+
+# variable "environment_instance_settings" {
+#   type = map(object({machine_type=string, tags=list(string)}))
+#   default = {
+#     "DEV" = {
+#       machine_type = "e2-micro"
+#       tags = ["dev"]
+#     },
+#     "QA" = {
+#       machine_type = "e2-small"
+#       tags = ["qa"]
+#     },
+#     "STAGE" = {
+#       machine_type = "e2-medium"
+#       tags = ["stage"]
+#     },
+#     "PROD" = {
+#       machine_type = "e2-standard-2"
+#       tags = ["prod"]
+#     }
+#   }
+# }
